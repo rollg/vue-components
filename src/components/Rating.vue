416a33d037405447bfffb4e107e68495899f5700
rollg <maildevcom@gmail.com>
2018-02-22 16:50:59 +0200

    initial commit;

---
+++ src/components/Rating.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="rating">
+    <ul class="list">
+      <li class="star active"><icon name="star"/></li>
+      <li class="star active"><icon name="star"/></li>
+      <li class="star active"><icon name="star"/></li>
+      <li class="star"><icon name="star-o"/></li>
+      <li class="star"><icon name="star-o"/></li>
+    </ul>
+    <span>3 of 5</span>
+  </div>
+</template>
+
+<script>
+  import 'vue-awesome/icons/star'
+  import 'vue-awesome/icons/star-o'
+
+  import Icon from 'vue-awesome/components/Icon'
+
+    export default {
+      name: "rating",
+      components: { Icon }
+    }
+</script>
+
+<style scoped>
+  .rating {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-size: 14px;
+    color: #a7a8a8;
+  }
+  .list {
+    margin: 0 0 5px 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  .list:hover .star {
+    color: #f3d23e;
+  }
+  .star {
+    display: inline-block;
+    cursor: pointer;
+  }
+  .star:hover ~ .star:not(.active) {
+    color: inherit;
+  }
+  .active {
+    color: #f3d23e;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Rating from './components/Rating'
+
+new Vue({
+  el: '#app',
+  template: '<Rating/>',
+  components: { Rating }
+})
